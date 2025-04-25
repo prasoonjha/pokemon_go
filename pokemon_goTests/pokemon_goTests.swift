6c93210d2e1fa2724df63b8c3445bf77720ade51
Prasoon Jha <38028423+prasoonjha@users.noreply.github.com>
2025-04-25 23:12:02 +0100

    Initial Commit

---
+++ pokemon_goTests/pokemon_goTests.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_goTests.swift
+//  pokemon_goTests
+//
+//  Created by Prasoon Jha on 25/04/2025.
+//
+
+import Testing
+@testable import pokemon_go
+
+struct pokemon_goTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ pokemon_goUITests/pokemon_goUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  pokemon_goUITestsLaunchTests.swift
+//  pokemon_goUITests
+//
+//  Created by Prasoon Jha on 25/04/2025.
+//
+
+import XCTest
+
+final class pokemon_goUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
